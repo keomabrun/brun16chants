9bfbadd5ce5e91d16f06de45d6f6f296d0a53f78
Keoma Brun <keoma.brun@inria.fr>
2016-07-04 13:56:14 +0200

    adding script to parse hrneighbors

---
+++ scripts/export_hrneighbors.py
@@ -0,0 +1,34 @@
+import json
+import influxdb
+import tools
+
+# open output file
+out_file = open('../data/hr_neighbors.csv','w')
+
+# configure influxDB
+influxClient = influxdb.client.InfluxDBClient(
+    host        = 'localhost',
+    port        = '8086',
+    database    = 'realms'
+)
+
+# query influxDB
+query       =   "SELECT * FROM SOL_TYPE_DUST_NOTIF_HRNEIGHBORS"
+query       +=  " WHERE site='ARG_junin' GROUP BY mac"
+json_list   = tools.influxdb_to_json(influxClient.query(query).raw)
+
+# write json to file
+for obj in json_list:
+    time    = tools.iso_to_epoch(obj["timestamp"])
+    mote_id = tools.mac_to_id(obj["mac"],time)
+
+    if mote_id is None:
+        pass
+
+    for key, value in obj["value"]["neighbors"].iteritems():
+            out_file.write(
+                time+','+\
+                str(mote_id)+','+\
+                str(value["rssi"])+\
+                '\n'
+            )
