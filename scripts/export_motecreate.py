a95d20a933d281164e831132cb1be99c198f0818
Keoma Brun <keoma.brun@inria.fr>
2016-07-04 13:55:44 +0200

    adding script to parse motecreate

---
+++ scripts/export_motecreate.py
@@ -0,0 +1,30 @@
+import json
+import influxdb
+import time
+import calendar
+import tools
+
+# open output file
+out_file = open('../data/motecreate.csv','w')
+
+# configure influxDB
+influxClient = influxdb.client.InfluxDBClient(
+    host        = 'localhost',
+    port        = '8086',
+    database    = 'realms'
+)
+
+# query influxDB
+query       =   "SELECT * FROM SOL_TYPE_DUST_EVENTMOTECREATE"
+query       +=  " WHERE site='ARG_junin' GROUP BY mac"
+json_list   = tools.influxdb_to_json(influxClient.query(query).raw)
+
+# write json to file
+out_file.write('time,mac,id\n')
+for obj in json_list:
+    out_file.write(
+        tools.iso_to_epoch(obj["timestamp"])+','+\
+        obj["value"]["macAddress"]+','+\
+        str(obj["value"]["moteId"])+\
+        '\n'
+    )
