559cf129920d05d0c58406cd22200184cc1af47c
Keoma Brun <keoma.brun@inria.fr>
2016-07-18 15:12:04 +0200

    adding lat long to dist function

--- scripts/tools.py
+++ scripts/tools.py
@@ -2,6 +2,7 @@ import flatdict
 import time
 import calendar
 import csv
+import math
 
 #-CONSTANTS-------------------------------------------------------------------#
 
@@ -102,7 +103,33 @@ def id_to_mac(mote_id, time):
                     break
     return mote_mac
 
+def distance_on_unit_sphere(lat1, long1, lat2, long2):
+    """
+    Code from John Cook.
+    http://www.johndcook.com/blog/python_longitude_latitude/
+    """
 
+    # Convert latitude and longitude to
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+
+    # Compute spherical distance from spherical coordinates.
+
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+        math.cos(phi1)*math.cos(phi2))
+    arc = math.acos( cos )
+
+    # Remember to multiply arc by the radius of the earth
+    # in your favorite set of units to get length.
+    return arc*6371*1000 # in meters
 
 
 
