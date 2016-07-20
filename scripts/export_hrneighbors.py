2d54ed55248014010740037e0daea2a1c7dfa756
Keoma Brun <keoma.brun@inria.fr>
2016-07-21 00:18:02 +0200

    adding board type to hrneighbors data file

--- scripts/export_hrneighbors.py
+++ scripts/export_hrneighbors.py
@@ -18,15 +18,19 @@ query       +=  " WHERE site='ARG_junin' GROUP BY mac"
 json_list   = tools.influxdb_to_json(influxClient.query(query).raw)
 
 # write json to file
-out_file.write("time,mac,neighborMac,neighborFlag,rssi,numTxPackets,numTxFailures,numRxPackets,distance\n")
+out_file.write("time,mac,neighborMac,neighborFlag,rssi,numTxPackets,numTxFailures,numRxPackets,mote_board,ngbr_board,distance\n")
 for obj in json_list:
-    time    = tools.iso_to_epoch(obj["timestamp"])
+    time                = tools.iso_to_epoch(obj["timestamp"])
     mote_lat, mote_long = tools.mac_to_position(obj["mac"],time)
+    mote_board          = tools.mac_to_board(obj["mac"],time)
+
+    if mote_board is None:
+        mote_board = ""
 
     for key, nghbr in obj["value"]["neighbors"].iteritems():
 
-        distance                = None
-        nghbr_mac               = tools.id_to_mac(nghbr["neighborId"],time)
+        distance                = -1
+        nghbr_mac, nghbr_board  = tools.id_to_mac(nghbr["neighborId"],time)
         nghbr_lat, nghbr_long   = tools.mac_to_position(nghbr_mac,time)
         if nghbr_lat is not None and mote_lat is not None:
             distance = tools.distance_on_unit_sphere(
@@ -35,19 +39,22 @@ for obj in json_list:
                         nghbr_lat,
                         nghbr_long
                     )
-        else: distance = None
 
-        nbr_mac = tools.id_to_mac(nghbr["neighborId"],time)
-        if nbr_mac is not None:
+        if nghbr_board is None:
+            nghbr_board = ""
+
+        if nghbr_mac is not None:
             out_file.write(
                 str(time)+','+\
                 str(obj["mac"])+','+\
-                str(nbr_mac)+','+\
+                str(nghbr_mac)+','+\
                 str(nghbr["neighborFlag"])+','+\
                 str(nghbr["rssi"])+','+\
                 str(nghbr["numTxPackets"])+','+\
                 str(nghbr["numTxFailures"])+','+\
                 str(nghbr["numRxPackets"])+','+\
+                mote_board+','+\
+                nghbr_board+','+\
                 str(distance)+\
                 '\n'
             )
